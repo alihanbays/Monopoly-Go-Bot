c9d3bebf28e5098d33838fcb7a9fa04e162a3993
alihanbaysal <baysala@oregonstate.edu>
2024-04-11 13:27:55 -0700

    4/11/2024

--- monopolygobot.py
+++ monopolygobot.py
@@ -7,6 +7,14 @@ goButtonPath = 'Buttons/GoButton.png'
 collectButtonPath = 'Buttons/CollectButton.png'
 shutdownButtonPath = 'Buttons/ShutDownButton.png'
 heistButtonPath = 'Buttons/HeistButton.png'
+megaHeistButtonPath = 'Buttons/MegaHeistButton.png'
+shutdown2ButtonPath = 'Buttons/ShutDown2Button.png'
+closeButtonPath = 'Buttons/CloseButton.png'
+okayButtonPath = 'Buttons/OkayButton.png'
+close2ButtonPath = 'Buttons/Close2Button.png'
+close3ButtonPath = 'Buttons/Close3Button.png'
+onItButtonPath = 'Buttons/OnItButton.png'
+jailButtonPath = 'Buttons/JailButton.png'
 
 def StartBot():
     while True:
@@ -35,7 +43,7 @@ def StartBot():
 
         # SHUTDOWN BUTTON   
         try:
-            shutdownButtonLocation = pyautogui.locateOnScreen(shutdownButtonPath, confidence=0.9)
+            shutdownButtonLocation = pyautogui.locateOnScreen(shutdownButtonPath, confidence=0.5)
             if shutdownButtonLocation:
                 print("I've found the go button! :)")
                 pyautogui.click(pyautogui.center(shutdownButtonLocation))
@@ -43,10 +51,21 @@ def StartBot():
                 print("I couldn't find the go button! :(")
         except pyautogui.ImageNotFoundException:
             print("I've raised an execption")
+        
+        # SHUTDOWN2 BUTTON   
+        try:
+            shutdown2ButtonLocation = pyautogui.locateOnScreen(shutdown2ButtonPath, confidence=0.5)
+            if shutdown2ButtonLocation:
+                print("I've found the go button! :)")
+                pyautogui.click(pyautogui.center(shutdown2ButtonLocation))
+            else:
+                print("I couldn't find the go button! :(")
+        except pyautogui.ImageNotFoundException:
+            print("I've raised an execption")
 
         # HEIST BUTTON   
         try:
-            heistButtonLocation = pyautogui.locateOnScreen(heistButtonPath, confidence=0.9)
+            heistButtonLocation = pyautogui.locateOnScreen(heistButtonPath, confidence=0.8)
             if heistButtonLocation:
                 print("I've found the go button! :)")
                 pyautogui.click(pyautogui.center(heistButtonLocation))
@@ -55,12 +74,87 @@ def StartBot():
         except pyautogui.ImageNotFoundException:
             print("I've raised an execption")
 
+        # MEGA HEIST BUTTON   
+        try:
+            megaHeistButtonLocation = pyautogui.locateOnScreen(megaHeistButtonPath, confidence=0.9)
+            if megaHeistButtonLocation:
+                print("I've found the go button! :)")
+                pyautogui.click(pyautogui.center(megaHeistButtonLocation))
+            else:
+                print("I couldn't find the go button! :(")
+        except pyautogui.ImageNotFoundException:
+            print("I've raised an execption")
+
+        # CLOSE BUTTON   
+        try:
+            closeButtonLocation = pyautogui.locateOnScreen(closeButtonPath, confidence=0.9)
+            if closeButtonLocation:
+                print("I've found the go button! :)")
+                pyautogui.click(pyautogui.center(closeButtonLocation))
+            else:
+                print("I couldn't find the go button! :(")
+        except pyautogui.ImageNotFoundException:
+            print("I've raised an execption")
+
+        # OKAY BUTTON   
+        try:
+            okayButtonLocation = pyautogui.locateOnScreen(okayButtonPath, confidence=0.9)
+            if okayButtonLocation:
+                print("I've found the go button! :)")
+                pyautogui.click(pyautogui.center(okayButtonLocation))
+            else:
+                print("I couldn't find the go button! :(")
+        except pyautogui.ImageNotFoundException:
+            print("I've raised an execption")
+
+        # CLOSE 2 BUTTON   
+        try:
+            close2ButtonLocation = pyautogui.locateOnScreen(close2ButtonPath, confidence=0.9)
+            if close2ButtonLocation:
+                print("I've found the go button! :)")
+                pyautogui.click(pyautogui.center(close2ButtonLocation))
+            else:
+                print("I couldn't find the go button! :(")
+        except pyautogui.ImageNotFoundException:
+            print("I've raised an execption")
+
+        # CLOSE 3 BUTTON   
+        try:
+            close3ButtonLocation = pyautogui.locateOnScreen(close3ButtonPath, confidence=0.9)
+            if close3ButtonLocation:
+                print("I've found the go button! :)")
+                pyautogui.click(pyautogui.center(close3ButtonLocation))
+            else:
+                print("I couldn't find the go button! :(")
+        except pyautogui.ImageNotFoundException:
+            print("I've raised an execption")
+
+        # OKAY BUTTON   
+        try:
+            onItButtonLocation = pyautogui.locateOnScreen(onItButtonPath, confidence=0.9)
+            if onItButtonLocation:
+                print("I've found the go button! :)")
+                pyautogui.click(pyautogui.center(onItButtonLocation))
+            else:
+                print("I couldn't find the go button! :(")
+        except pyautogui.ImageNotFoundException:
+            print("I've raised an execption")
+
+        # JAIL BUTTON   
+        try:
+            jailButtonLocation = pyautogui.locateOnScreen(jailButtonPath, confidence=0.9)
+            if jailButtonLocation:
+                print("I've found the go button! :)")
+                pyautogui.click(pyautogui.center(jailButtonLocation))
+            else:
+                print("I couldn't find the go button! :(")
+        except pyautogui.ImageNotFoundException:
+            print("I've raised an execption")
 
         # Check if Enter key is pressed to stop
         if keyboard.is_pressed('enter'):  
             print("Stopping actions.")
             break  # Break out of the loop to stop actions
-        time.sleep(1)
 
 
 print("Press Space To Start The Bot!")
